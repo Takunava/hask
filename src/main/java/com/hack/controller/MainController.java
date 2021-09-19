e1129e667535e21fc98a4c388de7d1f8efcb78f2
Denis Koltsov <DKoltsov@it-one.ru>
2021-09-19 16:01:20 +0300

    init

--- src/main/java/com/hack/controller/MainController.java
+++ src/main/java/com/hack/controller/MainController.java
@@ -3,6 +3,8 @@ package com.hack.controller;
 import com.hack.model.AppDto;
 import com.hack.service.AppService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +18,8 @@ public class MainController {
         this.service = service;
     }
 
-    public String saveApp(){
+    @PostMapping("/api/app")
+    public String saveApp(@RequestBody List<AppDto> apps){
         return null;
     }
 
--- src/main/java/com/hack/service/AppService.java
+++ src/main/java/com/hack/service/AppService.java
@@ -11,9 +11,9 @@ public class AppService {
 
     public List<AppDto> getApp() {
         return List.of(
-                new AppDto("TikTok", "TikTok", "https://cdn.worldvectorlogo.com/logos/tiktok-logo-2--1.svg", 3, new PermissionDto()),
-                new AppDto("VK", "VK", "https://worldvectorlogo.com/ru/download/vk-1.svg", 2, new PermissionDto()),
-                new AppDto("Instagram", "Instagram", "https://worldvectorlogo.com/ru/download/instagram-2-1.svg", 2, new PermissionDto()),
-                new AppDto("Telegram", "Telegram", "https://worldvectorlogo.com/ru/download/telegram.svg", 2, new PermissionDto()));
+                new AppDto("TikTok", "TikTok", "https://cdn.worldvectorlogo.com/logos/tiktok-logo-2--1.svg", 2, new PermissionDto()),
+                new AppDto("VK", "VK", "https://worldvectorlogo.com/ru/download/vk-1.svg", 1, new PermissionDto()),
+                new AppDto("Instagram", "Instagram", "https://worldvectorlogo.com/ru/download/instagram-2-1.svg", 1, new PermissionDto()),
+                new AppDto("Telegram", "Telegram", "https://worldvectorlogo.com/ru/download/telegram.svg", 0, new PermissionDto()));
     }
 }
